7b9f342387bed276e75d528fc52f0d9043bec5d8
Serge Stinckwich <Serge.Stinckwich@gmail.com>
2012-03-01 11:05:21 +0700

    bbayol

---
+++ src/ComputerBuilder.java
@@ -0,0 +1,16 @@
+public class ComputerBuilder 
+{
+
+    static public Computer computer()
+    {
+	return new Computer(null, null);
+    }
+    static public Computer scriptComputerWithoutDevices() 
+	{
+	    return computer();
+	}
+
+    }
+
+
+
--- src/FunctionSequenceDSLTest.java
+++ src/FunctionSequenceDSLTest.java
@@ -2,9 +2,30 @@ import junit.framework.Assert;
 import org.junit.Test;
 
 public class FunctionSequenceDSLTest {
+    /* DSL final
+    computer();
+      processor();
+        cores(2);
+        i386();
+      disk();
+        size(150);
+      disk()
+	size(75);
+        speed(7200);
+        sata();
+    */
 
 @Test
 public final void testTruth() {
 	Assert.assertEquals(true, true);
 	}
+
+@Test
+    public final void testComputerWithoutDevices()
+    {
+	Computer c = ComputerBuilder.scriptComputerWithoutDevices();
+       	Assert.assertEquals(c.getDisks(), null);
+       	//Assert.assertEquals(c.getProcessor(), null);
+    }
+
 }
